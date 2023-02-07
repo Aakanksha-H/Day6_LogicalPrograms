703c2363c921edeb7f1d1b7068e9d88f1742efe5
Aakanksha <123055596+Aakanksha-H@users.noreply.github.com>
2023-02-07 13:20:16 +0530

    Fabonacci Series

--- src/LogicalPrograms.java
+++ src/LogicalPrograms.java
@@ -1,5 +1,25 @@
+package com.bridgelabz.logicalprograms;
+
+import java.util.Scanner;
+
 public class LogicalPrograms {
     public static void main(String[] args) {
         System.out.println("Welcome to java logical progrmas");
+        fabonacciSeries();
+    }
+
+    public static void fabonacciSeries(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you till you want series");
+        int number = scanner.nextInt(), firstNumber = 0, secondNumber = 1;
+
+
+        while (firstNumber <= number){
+            System.out.println("Fibonacci Series till " + number + " is as follows:" + '\n' + firstNumber + " ,");
+
+            int nextTerm = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextTerm;
+        }
     }
 }
